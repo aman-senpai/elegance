eebf809a29ca5037001af0765c2e697aeb653cf5
aman-senpai <jemscollin1@gmail.com>
2023-10-15 00:28:06 +0530

    Finish landing page

---
+++ app/(marketing)/_components/heros.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+const Heros = () => {
+  return (
+    <div className="flex flex-col items-center justify-center max-w-5xl dark:bg-[#1F1F1F]">
+        <div className="flex items-center">
+            <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:h-[400px] md:w-[400px]">
+                <Image src="/documents.png"
+                fill
+                alt="document" className="object-contain dark:hidden" />
+                <Image src="/documents-dark.png"
+                fill
+                alt="document" className="object-contain hidden dark:block" />
+            </div>
+            <div className="relative h-[400px] w-[400px] hidden md:block">
+                <Image
+                src="/reading.png"
+                fill
+                alt="reading "
+                className="object-contain dark:hidden"
+                />
+                <Image
+                src="/reading-dark.png"
+                fill
+                alt="reading "
+                className="object-contain hidden dark:block"
+                />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Heros
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,17 @@
+import Footer from "./_components/footer"
+import Heading from "./_components/heading"
+import Heros from "./_components/heros"
+
+const MarketingPage = () => {
+  return (
+    <div className="min-h-full flex flex-col dark:bg-[#1F1F1F]">
+      <div className="flex flex-col items-center justify-center text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading />
+        <Heros /> 
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default MarketingPage
