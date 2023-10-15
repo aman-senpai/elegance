752caabbc795082d9270ed87f24e6485a8bd7d79
aman-senpai <jemscollin1@gmail.com>
2023-10-16 00:31:28 +0530

    Create schema, api for the document

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -4,9 +4,26 @@ import Image from "next/image"
 import { useUser } from "@clerk/clerk-react"
 import { Button } from "@/components/ui/button";
 import { PlusCircle } from "lucide-react";
+import { toast } from "sonner";
+
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api"
 
 const DocumentPage = () => {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({
+      title: "Kaho..."
+    })
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created",
+      error: "Failed to create a new note."
+    })
+  }
+
   return (
     <div className='h-full flex flex-col items-center justify-center space-y-4'>
       <Image
@@ -26,7 +43,7 @@ const DocumentPage = () => {
       <h2 className="text-lg font-medium">
         Welcome to {user?.firstName}&apos;s Elegance.
       </h2>
-      <Button>
+      <Button onClick={onCreate}>
         <PlusCircle className="h-4 w-4 mr-2" />
         Create a Note
       </Button>
