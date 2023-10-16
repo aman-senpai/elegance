d76c1a3e09c78fa990c8c688bccfbfbc66e40b53
aman-senpai <jemscollin1@gmail.com>
2023-10-16 23:41:48 +0530

    Add sidebar items

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,18 +1,25 @@
 "use client"
 
 import { cn } from '@/lib/utils'
-import { ChevronLeftIcon, MenuIcon } from 'lucide-react'
+
+import { ChevronLeftIcon, MenuIcon, PlusCircle, Search, Settings } from 'lucide-react'
+
 import { usePathname } from 'next/navigation'
 import React, { ElementRef, useEffect, useRef, useState } from 'react'
 import { useMediaQuery } from 'usehooks-ts'
 import UserItem from './user-item'
-import { useQuery } from 'convex/react'
+import Item from './item'
+
+import { toast } from "sonner";
+
+import { useQuery, useMutation } from 'convex/react'
 import { api } from '@/convex/_generated/api'
 
 const Navigation = () => {
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px");
-    // const documents = useQuery(api.documents.get);
+    const documents = useQuery(api.documents.get);
+    const create = useMutation(api.documents.create);
 
     const isResizingRef = useRef(false);
     const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -96,6 +103,17 @@ const Navigation = () => {
         }
     }
 
+    const handleCreate = () => {
+        const promise = create({
+            title: "Kaho..."
+        });
+
+        toast.promise(promise, {
+            loading: "Creating a new note...",
+            success: "New note created!",
+            error: "Failed to create a new note"
+        });
+    }
   return (
     <>
     <aside
@@ -112,14 +130,28 @@ const Navigation = () => {
         </div>
         <div>
             <UserItem />
+            <Item
+            label="Search"
+            icon={Search}
+            isSearch
+            onClick={() => {}}
+            />
+            <Item
+            label="Settings"
+            icon={Settings}
+            onClick={() => {}}
+            />
+            <Item
+            onClick={handleCreate}
+            label="New Page"
+            icon={PlusCircle} />
         </div>
         <div className='mt-4'>
-            {/* {documents?.map(document => {
+            {documents?.map(document => {
                 return (
                     <p key={document._id}>{document.title}</p>
                 )
-            })} */}
-            <p>Documents</p>
+            })}
         </div>
         <div
         onMouseDown={handleMouseDown}
--- convex/documents.ts
+++ convex/documents.ts
@@ -3,19 +3,19 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { Doc, Id } from "./_generated/dataModel";
 
-// export const get = query({
-//     handler: async (ctx) => {
-//         const identity = await ctx.auth.getUserIdentity();
+export const get = query({
+    handler: async (ctx) => {
+        const identity = await ctx.auth.getUserIdentity();
 
-//         if (!identity) {
-//             throw new Error("Not authenticated");
-//         }
+        if (!identity) {
+            throw new Error("Not authenticated");
+        }
 
-//         const documents = await ctx.db.query("documents").collect();
+        const documents = await ctx.db.query("documents").collect();
 
-//         return documents;
-//     }
-// })
+        return documents;
+    }
+})
 
 export const create = mutation({
     args: {
