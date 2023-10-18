7f8385fd12025c73e31369148ceacd66df45e5a7
aman-senpai <jemscollin1@gmail.com>
2023-10-18 23:49:04 +0530

    Finish archive functionality

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -13,8 +13,19 @@ import {
     LucideIcon,
     ChevronDown,
     ChevronRight,
-    Plus
+    Plus,
+    MoreHorizontal,
+    Trash
 } from "lucide-react";
+import {
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator
+
+ } from "@/components/ui/dropdown-menu";
+import { useUser } from "@clerk/clerk-react";
 
 interface ItemProps {
     id?: Id<"documents">;
@@ -41,8 +52,25 @@ const Item = ({
     onExpand,
     expanded,
 }: ItemProps) => {
+    const { user } = useUser();
     const router = useRouter();
     const create = useMutation(api.documents.create);
+    const archive = useMutation(api.documents.archive);
+
+    const onArchive = (
+        event: React.MouseEvent<HTMLDivElement, MouseEvent>
+    ) => {
+        event.stopPropagation();
+        if (!id)    return;
+        const promise = archive({id})
+
+        toast.promise(promise, {
+            loading: "Deleting...",
+            success: "Note archived",
+            error: "Failed to delete",
+        })
+    }
+
     const handleExpand = (
         event: React.MouseEvent<HTMLDivElement, MouseEvent>
     ) => {
@@ -112,10 +140,38 @@ const Item = ({
         )}
         {!!id && (
             <div
-            role="button"
-            onClick={onCreate}
             className="ml-auto flex items-center gap-x-2">
-                <div className="opacity-0 group-hover:opacity-100 ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600">
+                <DropdownMenu>
+                    <DropdownMenuTrigger
+                    onClick={(e) => e.stopPropagation()}
+                    asChild>
+                        <div
+                        role="button"
+                        className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600"
+                        >
+                            <MoreHorizontal className="h-4 w-4 text-muted-foreground" />
+                        </div>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent
+                    className="w-60"
+                    align="start"
+                    side="right"
+                    forceMount
+                    >
+                        <DropdownMenuItem onClick={onArchive}>
+                            <Trash className="h-4 w-4 mr-2" />
+                            Delete
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <div className="text-xs text-muted-foreground">
+                            Last edited by: {user?.fullName}
+                        </div>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+                <div
+                role="button"
+                onClick={onCreate}
+                className="opacity-0 group-hover:opacity-100 ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600">
                     <Plus className="h-4 w-4 text-muted-foreground" />
                 </div>
             </div>
