5079d215ffe75bc637c80869c8b182ba2fec2ab4
aman-senpai <jemscollin1@gmail.com>
2023-10-23 23:38:56 +0530

    Fetch documents in document id routes

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+"use client";
 
-const DocumentIdPage = () => {
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+
+import { useQuery } from "convex/react";
+
+interface DocumentIdProps {
+    params: {
+        documentId: Id<"documents">;
+    };
+}
+
+const DocumentIdPage = ({ params }: DocumentIdProps) => {
+    const document = useQuery(api.documents.getById, {
+        documentId: params.documentId,
+    });
+
+    if (document === undefined) return <div>Loading...</div>;
+
+    if (document === null) return <div>Not Found</div>;
+
     return (
-        <div>
-            <h1>DocumentId</h1>
+        <div className="bp-40">
+            <div className="md:max-w-3xl lg:md-max-w-4xl mx-auto">
+                DocumentId
+            </div>
         </div>
     );
 };
