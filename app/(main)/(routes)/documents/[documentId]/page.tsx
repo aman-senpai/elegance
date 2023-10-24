a51ae00091f8457512abd480cbdfa0c53176670b
aman-senpai <jemscollin1@gmail.com>
2023-10-25 01:55:52 +0530

    Create Editor use blocknote

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import Cover from "@/components/cover";
+import Editor from "@/components/editor";
 import Toolbar from "@/components/toolbar";
 import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 
 interface DocumentIdProps {
     params: {
@@ -18,6 +19,15 @@ const DocumentIdPage = ({ params }: DocumentIdProps) => {
     const document = useQuery(api.documents.getById, {
         documentId: params.documentId,
     });
+
+    const update = useMutation(api.documents.update);
+
+    const onChange = (content: string) => {
+        update({
+            id: params.documentId,
+            content,
+        });
+    };
 
     if (document === undefined) {
         return (
@@ -42,6 +52,10 @@ const DocumentIdPage = ({ params }: DocumentIdProps) => {
             <Cover url={document.coverImage} />
             <div className="md:max-w-3xl lg:md-max-w-4xl mx-auto">
                 <Toolbar initialData={document} />
+                <Editor
+                    onChange={onChange}
+                    initialContent={document.content}
+                />
             </div>
         </div>
     );
